6cdba0b09e3d1b61b81369f52a912aa21f620e43
soudai256 <100189108+soudai256@users.noreply.github.com>
2022-02-25 14:37:58 +0900

    セクション4おわり

--- src/index.js
+++ src/index.js
@@ -157,3 +157,75 @@ console.log(arr4);
 /**
  * mapやfilterを使った配列の処理
  */
+
+//const nameArr = ["かす", "ちんこ", "きんたま"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index+1}番目は${nameArr[index]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name)=> {
+//   return name;
+// })
+// console.log(nameArr2);
+
+//nameArr.map((name, index) => console.log(`${index+1}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+
+// console.log(newNumArr);
+
+// const newNameArr= nameArr.map((name) => {
+//   if(name==="ちんこ") {
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+//ある条件　? 条件がtrueの時 : 条件がfalseの時
+//const val1 = 1>0 ? "trueです" : "falseです";
+//console.log(val1);
+
+// const num=1300;
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num =="number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています！！" : "許容範囲内です";
+};
+
+console.log(checkSum(23, 438));
+
+/**
+ * 論理演算氏の本当の意味を知ろう  && ||
+ */
+// const flag1 =true;
+// const flag2=true;
+
+// if (flag1||flag2) {
+//   console.log("1か2はtrueになります");
+// }
+
+// if (flag1&&flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+//||は左側がfalseなら右側を返す
+const num3 = null;
+const fee = num3 || "金額未設定です";
+console.log(fee);
+
+//&&は左側がtrueなら右側を返す
+const num4 = 100;
+const fee4 = num4 && "何か設定されました";
+console.log(fee4);
